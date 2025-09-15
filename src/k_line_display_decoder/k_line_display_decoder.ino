c7120e47d0d0744de5ac9587f8b510f9d4e142cd
wish4snow <wish4snow57@gmail.com>
2025-09-15 12:58:20 -0500

    added function for rpm meter

--- src/k_line_display_decoder/k_line_display_decoder.ino
+++ src/k_line_display_decoder/k_line_display_decoder.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 
 const int LED_ARRAY [10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+const int led_amount = 11;
 unsigned long current_time = 0;
 unsigned long previous_time = 0;
 int flash_state;
@@ -44,5 +45,25 @@ void led_flash (int LED_ARRAY [], int interval, unsigned long current_time = cur
 		for (int i = 0; i < 10; i++) {
 			digitalWrite(LED_ARRAY[i], flash_state);
 		}
+	}
+}
+
+void led_rpm_meter (int LED_ARRAY [], int rpm, unsigned long current_time, unsigned long *previous_time) {
+
+	int idle_rpm = 900;
+	int flash_rpm = 4000;
+	int flash_interval = 500; // in milliseconds
+	
+	if (rpm < flash_rpm) {
+		for (int i = 0; i < led_amount; i++) {
+			if (i < rpm % ((flash_rpm - idle_rpm) / led_amount)) {
+
+				digitalWrite (LED_ARRAY[i], HIGH);
+			} else {
+				digitalWrite ( LED_ARRAY, LOW );
+			}
+		}
+	} else {
+		led_flash (LED_ARRAY, flash_interval, current_time, previous_time);
 	}
 }
