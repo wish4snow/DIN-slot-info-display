6042ea0f28ac4a98a8238519cd93996343783183
wess <wish4snow57@gmail.com>
2026-03-11 15:25:40 -0500

    way to check supported pids added

--- src/k_line_display_decoder/k_line_display_decoder.ino
+++ src/k_line_display_decoder/k_line_display_decoder.ino
@@ -2,19 +2,22 @@
 #include <OBD2_KLine.h>
 #include <LiquidCrystal_I2C.h>
 
-const int LED_ARRAY [10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+// LED bar global varibles
+const int LED_ARRAY_PINS [] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 const int led_amount = 11;
 unsigned long current_time = 0;
 unsigned long previous_time = 0;
 int flash_state;
 
+// k line global varibles 
 AltSoftSerial alt_serial;
-OBD2_KLine KLine(alt_serial, (uint32_t)10400, 0, 1);  // Uses Hardware Serial (Serial1) at 10400 baud, with RX on pin 10 and TX on pin 11.
+OBD2_KLine KLine(alt_serial, 10400, 0, 1);  // Uses Hardware Serial (Serial1) at 10400 baud, with RX on pin 10 and TX on pin 11.
+bool supported_pids [200];
 
 void setup() {
 	// setting led pins
 	for (int i = 0; i < 10; i++) {
-		pinMode(LED_ARRAY[i], OUTPUT);
+		pinMode(LED_ARRAY_PINS[i], OUTPUT);
 	}
 
 	//I2C LCD setup
@@ -41,11 +44,15 @@ void setup() {
 	if (KLine.initOBD2()) {
 		int liveDataLength = KLine.readSupportedLiveData();  // Read supported live data PIDs. Mode: 01
 		if (liveDataLength > 0) {
+      Serial.println(liveDataLength);
 			Serial.print("LiveData: ");
 			for (int i = 0; i < liveDataLength; i++) {
 				byte supported = KLine.getSupportedData(0x01, i);  // Get supported live data PID
 				Serial.print(supported, HEX);                      // Print the PID in hexadecimal format
 				Serial.print(" ");
+
+        supported_pids[i] = (bool)supported;
+        
 			}
 			Serial.println();
 		} else {
@@ -58,7 +65,7 @@ void loop() {
 
 }
 
-void led_flash (int LED_ARRAY [], int interval, unsigned long current_time = current_time, unsigned long *previous_time = &previous_time) {
+void led_flash (int LED_ARRAY_PINS [], int interval, unsigned long current_time = current_time, unsigned long *previous_time = &previous_time) {
 
 	if (current_time >= *previous_time + interval) {
 		*previous_time = current_time;
@@ -72,12 +79,12 @@ void led_flash (int LED_ARRAY [], int interval, unsigned long current_time = cur
 		}
 
 		for (int i = 0; i < 10; i++) {
-			digitalWrite(LED_ARRAY[i], flash_state);
+			digitalWrite(LED_ARRAY_PINS[i], flash_state);
 		}
 	}
 }
 
-void led_rpm_meter (int LED_ARRAY [], int rpm, unsigned long current_time, unsigned long *previous_time) {
+void led_rpm_meter (int LED_ARRAY_PINS [], int rpm, unsigned long current_time, unsigned long *previous_time) {
 
 	int idle_rpm = 900;
 	int flash_rpm = 4000;
@@ -87,12 +94,12 @@ void led_rpm_meter (int LED_ARRAY [], int rpm, unsigned long current_time, unsig
 		for (int i = 0; i < led_amount; i++) {
 			if (i <= (0 - led_amount) / (idle_rpm - flash_rpm) * (rpm - idle_rpm)) { // equation is based on a linear graph in point slope form: y = m(x - x1) + y1
 
-					digitalWrite (LED_ARRAY[i], HIGH);
+					digitalWrite (LED_ARRAY_PINS[i], HIGH);
 					} else {
-					digitalWrite ( LED_ARRAY[i], LOW );
+					digitalWrite ( LED_ARRAY_PINS[i], LOW );
 					}
 					}
 					} else {
-					led_flash (LED_ARRAY, flash_interval, current_time, previous_time);
+					led_flash (LED_ARRAY_PINS, flash_interval, current_time, previous_time);
 					}
 					}
