0ad57c03a39427173cb005fa7a2f85dd31ea7104
wish4snow <wish4snow57@gmail.com>
2026-05-11 15:37:32 -0500

    fixed led functions, need to deside if want to merge with master

---
+++ src/k_line_display_decoder/led_bar.h
@@ -0,0 +1,38 @@
+void led_flash (int LED_ARRAY_PINS [], int interval, int flash_state, unsigned long current_time, unsigned long *previous_time) {
+
+	if (current_time >= *previous_time + interval) {
+		*previous_time = current_time;
+
+		if (flash_state == HIGH) {
+
+			flash_state = LOW;
+		} else {
+
+			flash_state = HIGH;
+		}
+
+		for (int i = 0; i < 11; i++) {
+			digitalWrite(LED_ARRAY_PINS[i], flash_state);
+		}
+	}
+}
+
+void led_rpm_meter (int LED_ARRAY_PINS [], int rpm, int flash_state, unsigned long current_time, unsigned long *previous_time) {
+
+	int idle_rpm = 900;
+	int flash_rpm = 4000;
+	int flash_interval = 500; // in milliseconds
+
+	if (rpm < flash_rpm) {
+		for (int i = 0; i < 11; i++) {
+			if (i <= (int) ((float) (0 - 11) / (idle_rpm - flash_rpm) * (rpm - idle_rpm))) { // equation is based on a linear graph in point slope form: y = m(x - x1) + y1
+
+				digitalWrite (LED_ARRAY_PINS[i], HIGH);
+			} else {
+				digitalWrite ( LED_ARRAY_PINS[i], LOW );
+			}
+		}
+	} else {
+		led_flash (LED_ARRAY_PINS, flash_interval, flash_state, current_time, previous_time);
+	}
+}
