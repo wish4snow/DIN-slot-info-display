9948440a954378e675b8bbc15b08b9eb27a5e03f
wish4snow <wish4snow57@gmail.com>
2026-05-11 20:20:55 -0500

    made functions more readable

--- src/k_line_display_decoder/led_bar.h
+++ src/k_line_display_decoder/led_bar.h
@@ -2,7 +2,8 @@ void led_flash (int LED_ARRAY_PINS [], int interval, int flash_state, unsigned l
 
 	if (current_time >= *previous_time + interval) {
 		*previous_time = current_time;
-
+		flash_state = (flash_state == HIGH) ? LOW : HIGH;
+		/*
 		if (flash_state == HIGH) {
 
 			flash_state = LOW;
@@ -10,7 +11,7 @@ void led_flash (int LED_ARRAY_PINS [], int interval, int flash_state, unsigned l
 
 			flash_state = HIGH;
 		}
-
+		*/
 		for (int i = 0; i < 11; i++) {
 			digitalWrite(LED_ARRAY_PINS[i], flash_state);
 		}
@@ -25,12 +26,16 @@ void led_rpm_meter (int LED_ARRAY_PINS [], int rpm, int flash_state, unsigned lo
 
 	if (rpm < flash_rpm) {
 		for (int i = 0; i < 11; i++) {
+
+			digitalWrite(LED_ARRAY_PINS[i], (i <= (int) ((float) (0 - 11) / (idle_rpm - flash_rpm) * (rpm - idle_rpm))) ? HIGH : LOW);
+			/*
 			if (i <= (int) ((float) (0 - 11) / (idle_rpm - flash_rpm) * (rpm - idle_rpm))) { // equation is based on a linear graph in point slope form: y = m(x - x1) + y1
 
 				digitalWrite (LED_ARRAY_PINS[i], HIGH);
 			} else {
 				digitalWrite ( LED_ARRAY_PINS[i], LOW );
 			}
+			*/
 		}
 	} else {
 		led_flash (LED_ARRAY_PINS, flash_interval, flash_state, current_time, previous_time);
