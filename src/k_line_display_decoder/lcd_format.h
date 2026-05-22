400e1ee0a460aa0cc42d82d9bf1199f231b874bf
wess <wish4snow57@gmail.com>
2026-05-21 23:54:49 -0500

    fixed and texted write function. need better name

--- src/k_line_display_decoder/lcd_format.h
+++ src/k_line_display_decoder/lcd_format.h
@@ -1,9 +1,9 @@
 #include <LiquidCrystal_I2C.h>
 
-void write (LiquidCrystal_I2C screen_object, int row, String left_text, String right_text) {
+void write (LiquidCrystal_I2C screen_object, int row, String left_text, String right_text, int colum_length = 16) {
 	screen_object.setCursor(0, row);
 	screen_object.print(left_text);
 
-	screen_object.setCursor(left_text.length(), row);
+	screen_object.setCursor(colum_length - right_text.length(), row);
 	screen_object.print(right_text);
 }
