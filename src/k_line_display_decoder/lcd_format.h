83bc8d37750dde0a0302a571841bc0ef14b28846
wess <wish4snow57@gmail.com>
2026-05-21 23:47:40 -0500

    finished screen formater

--- src/k_line_display_decoder/lcd_format.h
+++ src/k_line_display_decoder/lcd_format.h
@@ -1,5 +1,9 @@
 #include <LiquidCrystal_I2C.h>
 
 void write (LiquidCrystal_I2C screen_object, int row, String left_text, String right_text) {
+	screen_object.setCursor(0, row);
+	screen_object.print(left_text);
 
+	screen_object.setCursor(left_text.length(), row);
+	screen_object.print(right_text);
 }
