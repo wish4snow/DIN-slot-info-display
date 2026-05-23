e4d71adbcf27b887937e20a9a12d2df860e482e4
wess <wish4snow57@gmail.com>
2026-05-22 22:44:24 -0500

    made value formater for displaying values. future note, test static keyword for previous_time and current_time in led_bar

--- src/k_line_display_decoder/lcd_format.h
+++ src/k_line_display_decoder/lcd_format.h
@@ -2,8 +2,12 @@
 
 void write (LiquidCrystal_I2C screen_object, int row, String left_text, String right_text, int colum_length = 16) {
 	screen_object.setCursor(0, row);
-	screen_object.print(left_text);
+	screen_object.print(left_text + ":");
 
 	screen_object.setCursor(colum_length - right_text.length(), row);
 	screen_object.print(right_text);
+}
+
+String format_value (int value, String unit_abbreviation) {
+	return ((String) value + unit_abbreviation);
 }
