900c57826709555e79f30c6f94d8aa97842be465
wish4snow <wish4snow57@gmail.com>
2026-07-15 12:23:14 -0500

    started code for kwp-2000 obd protocal

---
+++ src/kwp-2000_decoder/kwp-2000_decoder.ino
@@ -0,0 +1,79 @@
+#include <AltSoftSerial.h>
+#include <OBD2_KLine.h>
+#include "led_bar.h"
+#include "lcd_format.h"
+
+// LED bar global varibles
+const int LED_ARRAY_PINS [] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+const int led_amount = 11;
+
+// k line global varibles 
+AltSoftSerial alt_serial;
+OBD2_KLine KLine(alt_serial, 10400, 0, 1);  // Uses Hardware Serial (Serial1) at 10400 baud, with RX on pin 0 and TX on pin 1.
+bool supported_pids [200];
+int user_defined_pids [5] = {0x0C, 0x0B, 0x0A, 0x0F, 0x11}; // defined by user, will make better way to interact with in future
+
+void setup() {
+
+	// setting led pins
+	for (int i = 0; i < 11; i++) {
+		pinMode(LED_ARRAY_PINS[i], OUTPUT);
+	}
+	
+	//turns rpm fully off when setup code is initalized
+	led_rpm_meter (LED_ARRAY_PINS, 0);
+
+	//I2C LCD setup
+	LiquidCrystal_I2C lcd_left (0x27,16,2);
+	LiquidCrystal_I2C lcd_right (0x26,16,2);
+
+	lcd_left.init();
+	lcd_right.init();
+
+	lcd_left.backlight();
+	lcd_right.backlight();
+
+	Serial.begin(115200);  // Start the default serial (for logging/debugging)
+	Serial.println("OBD2 K-Line Get Supported PIDs Example");
+
+	KLine.setDebug(Serial);          // Optional: outputs debug messages to the selected serial port
+	KLine.setProtocol("Automatic");  // Optional: communication protocol (default: Automatic; supported: ISO9141, ISO14230_Slow, ISO14230_Fast, Automatic)
+	KLine.setByteWriteInterval(5);   // Optional: delay (ms) between bytes when writing
+	KLine.setInterByteTimeout(60);   // Optional: sets the maximum inter-byte timeout (ms) while receiving data
+	KLine.setReadTimeout(1000);      // Optional: maximum time (ms) to wait for a response after sending a request
+
+	Serial.println("OBD2 Starting.");
+
+	if (KLine.initOBD2()) {
+
+		int liveDataLength = KLine.readSupportedLiveData();  // Read supported live data PIDs. Mode: 01
+		
+		if (liveDataLength > 0) {
+	
+			Serial.println(liveDataLength);
+			Serial.print("LiveData: ");
+			
+			for (int i = 0; i < liveDataLength; i++) {
+
+				byte supported = KLine.getSupportedData(0x01, i);  // Get supported live data PID
+				Serial.print(supported, HEX);                      // Print the PID in hexadecimal format
+				Serial.print(" ");
+
+				supported_pids[i] = (bool)supported;
+			
+			}
+		
+			Serial.println();
+
+		} else {
+			Serial.print("LiveData not supported!");
+		}
+	}
+	Serial.println("end");
+}
+void loop() {
+	led_rpm_meter (LED_ARRAY_PINS, 5000);
+	// put your main code here, to run repeatedly:
+
+}
+
---
+++ src/kwp-2000_decoder/led_bar.h
@@ -0,0 +1,37 @@
+void led_flash (const int LED_ARRAY_PINS [], int interval) {
+
+	static unsigned long current_time = 0;
+	static unsigned long previous_time = 0;
+	static int flash_state = LOW;
+
+	current_time = millis();
+
+	if (current_time >= previous_time + interval) {
+
+		previous_time = current_time;
+		flash_state = (flash_state == HIGH) ? LOW : HIGH;
+
+		for (int i = 0; i < 11; i++) {
+
+			digitalWrite(LED_ARRAY_PINS[i], flash_state);
+		}
+	}
+}
+
+void led_rpm_meter (const int LED_ARRAY_PINS [], int rpm) {
+
+	int idle_rpm = 900;
+	int flash_rpm = 4000;
+	int flash_interval = 500; // in milliseconds
+
+	if (rpm < flash_rpm) {
+		for (int i = 0; i < 11; i++) {
+
+			// equation is based on a linear graph in point slope form: y = m(x - x1) + y1
+			digitalWrite(LED_ARRAY_PINS[i], (i <= (int) ((float) (0 - 11) / (idle_rpm - flash_rpm) * (rpm - idle_rpm))) ? HIGH : LOW);
+		}
+
+	} else {
+		led_flash (LED_ARRAY_PINS, flash_interval);
+	}
+}
